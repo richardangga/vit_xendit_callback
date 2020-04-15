1c62331e7b587a8ec4df4c16cb5b8dd92918fb8a
root <root@ip-172-31-25-156.ap-southeast-1.compute.internal>
2020-04-15 08:24:01 +0000

    update

--- models/models.py
+++ models/models.py
@@ -2,14 +2,7 @@
 
 from odoo import models, fields, api
 
-# class vit_xendit_callback(models.Model):
-#     _name = 'vit_xendit_callback.vit_xendit_callback'
+#class SalesOrderInherit(models.Model):
+#	_inherit = 'sale.order'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+#	external_id = fields.Char('External ID')
